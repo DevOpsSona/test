fd4d0ed72eefe8f75dc0f3c6962e9155c7fff1a9
Tanya <102754510+Pasechnyktanya@users.noreply.github.com>
2023-08-14 22:29:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-a const express = require("express");
+const express = require("express");
 const bodyParser = require("body-parser");
 const sqlite3 = require("sqlite3").verbose();
 const cors = require("cors");
